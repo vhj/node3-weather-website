a196fe8f5d8b455205e305e74a8e13568af1cc83
Vimal Jani <vimal@altair.com>
2020-08-03 16:41:23 -0700

    Remove unnecessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,3 @@
-console.log('Client side javascript file is loaded!')
-
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
@@ -11,7 +8,7 @@ weatherForm.addEventListener('submit', (e)=>{
     const location = search.value
     const url = '/weather?address='+location
     messageOne.textContent = 'Loading...'
-messageTwo.textContent = ''
+    messageTwo.textContent = ''
     fetch(url).then((response)=>{
         response.json().then((data) =>{
             if(data.error){

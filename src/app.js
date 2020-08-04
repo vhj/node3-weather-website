e1639bd4c53bd2b09d7a9cbcabfd02c7dfc70401
Vimal Jani <vimal@altair.com>
2020-08-03 17:27:44 -0700

    Setup app for Heroku

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 const path = require ('path')
 const express = require('express')
 const hbs = require('hbs')  //Only need it for partials
-const app = express()
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
+
+const app = express()
+const port = process.env.PORT || 3001
 
 //Define paths for Express config
 const publicDirectoryPath = path.join(__dirname,'../public')
@@ -86,6 +88,6 @@ app.get('*',(req,res) =>{
         errorMsg: 'Page Not found'
     })
 })
-app.listen(3001, () =>{
-     console.log('Server is up on port 3001.')
+app.listen(port, () =>{
+     console.log('Server is up on port '+port+'.')
 })
